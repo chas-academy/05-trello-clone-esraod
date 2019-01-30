da49fc836d526fa981049adff516e660c1f4322c
Esra Oktav Dinler <oktavesra@gmail.com>
2019-01-30 12:45:38 +0100

    Effects added for the delete card and list

--- src/js/app.js
+++ src/js/app.js
@@ -59,7 +59,7 @@ const jtrello = (function() {
 
     // variable for the list creation title input field's value
     var valueListCreation = $('#list-creation-dialog').find('input').val();
-
+    
     // variable with the elements for creating a new List / Column with the new value given from the input field for the list title
     var columnList = ('<div class="column"><div class="list"><div class="list-header">'+ valueListCreation +'<button class="button delete">X</button></div><ul class="list-cards"><li class="add-new"><form class="new-card" action="index.html"><input type="text" name="title" placeholder="Please name the new card" /><button class="button add">Add new card</button></form></li></ul></div></div>');
 
@@ -68,6 +68,7 @@ const jtrello = (function() {
     
     // Set the value of the list creation input to null after submit (clears the input field)
     $('#list-creation-dialog').find('input').val(null);
+      
   }
 
 
@@ -75,7 +76,22 @@ const jtrello = (function() {
   function deleteList() {
     console.log("This deletes the list you clicked on");
 
+   $(this).parentsUntil('.board').fadeOut(900, function() {
     $(this).parentsUntil('.board').remove();
+  });
+
+/*
+    // Deletes list when doubleclicked on delete button
+    $('.column').on('dblclick', '.delete', function () {
+      $(this).parentsUntil('.board').remove();
+    });
+
+
+    // Fades out the list when click on delete button
+    $(this).parentsUntil('.board').fadeTo(200, 1.4, function() {
+      $(this).parentsUntil('.board')
+    });
+*/
   }
 
 
@@ -101,7 +117,16 @@ const jtrello = (function() {
   function deleteCard() {
     console.log("This deletes the card you clicked on");
 
-    $(this).parent('.card').remove();
+    // Deletes card when doubleclicked on delete button
+    $('.list').on('dblclick', '.delete', function () {
+      $(this).parent('.card').remove();
+    });
+
+
+    // Fades out the card when clicked on delete button
+    $(this).parent('.card').fadeTo(200, 0.4, function() {
+      $(this).parent('.card')
+    });
   }
 
  
